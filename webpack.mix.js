7cbd359f158bfe01ec3c99d7e1432bd8a018e334
Soladiem <sitko.dv@gmail.com>
2020-01-29 11:42:04 +0300

    Update main page and resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.sass('resources/sass/start.scss', 'public/css');
+
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
