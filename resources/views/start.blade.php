81c37489bcb44d36d3f19695d4f2d1b1703c5bea
Soladiem <sitko.dv@gmail.com>
2020-01-29 18:49:29 +0300

    Add show / hide password icon

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -11,10 +11,12 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/start.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 <body>
-
-<div class="wrapper">
+<div class="wrapper" id="app">
     @if (Route::has('login'))
         <div class="wrapper__menu links">
             @auth
@@ -32,35 +34,9 @@
     <div class="wrapper__center">
         <div class="container">
             <h1>{{ config('app.name', 'AlacrisCRM') }}</h1>
-
-            <form method="POST" action="{{ route('login') }}">
-                <input type="email"
-                       class="form-input"
-                       name="email"
-                       value="{{ old('email') }}"
-                       placeholder="Email"
-                       required
-                       autocomplete="email">
-                <input type="password"
-                       class="form-input"
-                       name="password"
-                       placeholder="Password"
-                       required
-                       autocomplete="current-password">
-
-                <label class="form-check">{{ __('Remember Me') }}
-                    <input type="checkbox"
-                           name="remember"
-                           id="remember" {{ old('remember') ? 'checked' : '' }}>
-                    <span class="form-check-mark"></span>
-                </label>
-
-                <button type="submit">
-                    {{ __('Login') }}
-                </button>
-            </form>
+            <login-form></login-form>
             <div class="copyright">
-                &copy; AlacrisCRM, 2020
+                &copy; {{ config('app.name', 'AlacrisCRM') }}, 2020
             </div>
         </div>
         <ul class="bg-bubbles">
