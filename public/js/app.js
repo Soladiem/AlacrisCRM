937d9a4e91e5bb25d09223920724b455a7bb629b
Soladiem <sitko.dv@gmail.com>
2020-01-29 11:41:34 +0300

    Add app.js

--- public/js/app.js
+++ public/js/app.js
@@ -49654,15 +49654,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/start.scss":
+/*!***********************************!*\
+  !*** ./resources/sass/start.scss ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/start.scss ./resources/sass/app.scss ***!
+  \*****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\ospanel\domains\alacris.loc\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\ospanel\domains\alacris.loc\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\OSPanel\domains\alacris.loc\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\OSPanel\domains\alacris.loc\resources\sass\start.scss */"./resources/sass/start.scss");
+module.exports = __webpack_require__(/*! D:\OSPanel\domains\alacris.loc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
