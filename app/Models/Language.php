b176cf8d43e56f88bd70a04828b26320a227649d
Soladiem <sitko.dv@gmail.com>
2020-02-04 13:32:47 +0300

    Update Language model

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -36,5 +37,8 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Language extends Model
 {
-    //
+    public function scopeActive(Builder $query)
+    {
+        return $query->where('status', '=', 1);
+    }
 }
