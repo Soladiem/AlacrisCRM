24275637efc2d3578d6a36454bec63e1bb39e9a3
Soladiem <sitko.dv@gmail.com>
2020-02-06 17:01:24 +0300

    Change Route::redirect() by Redirect::to()

--- app/Http/Middleware/LocaleMiddleware.php
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -7,6 +7,7 @@ use App\Exceptions\UnsupportedLocaleException;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Redirect;
 use Str;
 
 class LocaleMiddleware
@@ -115,7 +116,7 @@ class LocaleMiddleware
 
             // Redirect only for exists routes
             if ($routeExists) {
-                Route::redirect(request()->getPathInfo(), '/' . $requestUri);
+                return Redirect::to('/' . $requestUri);
             }
         }
 
