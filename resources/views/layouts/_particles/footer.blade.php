196946c450693860705a7c9f90273549150c8b3c
Soladiem <sitko.dv@gmail.com>
2020-02-17 13:53:30 +0300

    Update default app layout

---
+++ resources/views/layouts/_particles/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="footer-block buttons"></div>
+    <div class="footer-block author">
+        <ul>
+            <li> &copy; <a href="//sitkodenis.ru">
+                    {{ config('app.name') }}
+                </a>
+            </li>
+        </ul>
+    </div>
+</footer>
