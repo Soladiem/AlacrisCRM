63a22fc132e3135f0bcc42cc31a7a1e871b4ef56
Soladiem <sitko.dv@gmail.com>
2020-02-27 20:24:34 +0300

    Update sidebar.scss

--- modules/Dashboard/Resources/views/index.blade.php
+++ modules/Dashboard/Resources/views/index.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('dashboard.name') !!}
-    </p>
+    <h1>Dashboard</h1>
 @endsection
